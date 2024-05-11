f12e6bfe291be5e0169a5ae75dbb6580f42c6afb
Wojtek <your.email@example.com>
2024-05-11 13:24:32 +0200

    first commit

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,32 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if third all slides contain "title and description"', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-slide-active').should('contain', 'Rome')
+      .and('contain', 'Italy');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'London')
+      .and('contain', 'United Kingdom')  ;
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Paris')
+      .and('contain', 'France');
+  });
+});
+
+describe('Swiper Gallery Navigation Test', function () {
+  it('Allows the user to navigate slides using next and previous buttons', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('be.visible'); 
+    cy.get('.swiper-button-prev').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('be.visible');
+  });
+});
